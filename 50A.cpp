2f6c963d5ae7af39c072e61efe5fe82d41597ab4
priyanshu-rathore-slt <priyanshu@sltechnocrats.net>
2025-08-22 11:34:10 +0530

    uc

---
+++ 50A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define int long long
+
+int32_t main() {
+    fastio;
+    
+    int a,b; cin >> a >> b;
+
+    int max_value = a * b;
+
+    cout << max_value/2 << endl;
+    
+    
+    return 0;
+}
